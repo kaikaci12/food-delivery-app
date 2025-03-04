a759036d75e4f4180c5de97f68b308dedd192cf7
kaikaci12 <somewonesomewone@gmail.com>
2025-03-04 22:57:12 +0400

    feat: add listener of the database, when the user doc is changed we instantly update the user of authstate to get up to date data

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -7,9 +7,10 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
+  signOut,
 } from "firebase/auth";
 import { db } from "@/firebaseConfig";
-import { getDoc, setDoc, doc } from "firebase/firestore";
+import { getDoc, setDoc, doc, onSnapshot } from "firebase/firestore";
 const AuthProvider = ({ children }: any) => {
   const [authState, setAuthState] = useState<{
     token: string | null;
@@ -18,7 +19,34 @@ const AuthProvider = ({ children }: any) => {
     token: null,
     user: null,
   });
+  useEffect(() => {
+    const unsubscribeAuth = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        try {
+          const token = await user.getIdToken();
+          const userRef = doc(db, "users", user.uid);
 
+          // Subscribe to real-time Firestore updates
+          const unsubscribeFirestore = onSnapshot(userRef, (docSnap) => {
+            if (docSnap.exists()) {
+              setAuthState({
+                token,
+                user: { uid: user.uid, email: user.email, ...docSnap.data() },
+              });
+            }
+          });
+
+          return () => unsubscribeFirestore();
+        } catch (error: any) {
+          console.log(error.message);
+        }
+      } else {
+        setAuthState({ token: null, user: null });
+      }
+    });
+
+    return () => unsubscribeAuth(); // Cleanup auth listener
+  }, []);
   const register = async (
     username: string,
     email: string,
@@ -45,8 +73,29 @@ const AuthProvider = ({ children }: any) => {
       console.log(error.messsage);
     }
   };
-  const login = (username: string, password: string) => {};
-  const logOut = () => {};
+  const login = async (email: string, password: string) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      setAuthState({
+        token: null,
+        user: null,
+      });
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
 
   const value = {
     onRegister: register,
